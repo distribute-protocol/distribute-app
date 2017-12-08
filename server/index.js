ea0d9ff8e5950846f9a486514f42ec100c416568
Jessica Marshall <jessica.marshall14@gmail.com>
2017-12-08 11:56:06 -0500

    server scrapes event logs now

--- server/index.js
+++ server/index.js
@@ -4,12 +4,32 @@ const path = require('path')
 const ObjectId = require('mongodb').ObjectID
 const MongoClient = require('mongodb').MongoClient
 const assert = require('assert')
+
 const Eth = require('ethjs')
-const eth = new Eth(new Eth.HttpProvider('HTTP://127.0.0.1:7545'))
+const eth = new Eth(new Eth.HttpProvider('http://localhost:7545'))
+const Web3 = require('web3')
+const web3 = new Web3(new Web3.providers.HttpProvider('http://localhost:7545'))
+
 
 eth.accounts().then(accountsArr => {
   console.log(accountsArr)
 })
+
+//
+const filter = web3.eth.filter({
+  fromBlock: 0,
+  toBlock: 'latest',
+  //address is totalCapitalTokenAddress from ../src/abi/TokenHolderRegistry.js
+  address: '0x8f0483125fcb9aaaefa9209d8e9d7b9c8b9fb90f',
+  topics: [web3.sha3('LogProposal(uint256,uint256)')]
+})
+
+filter.watch((error, result) => {
+   console.log(result)
+})
+//
+
+
 const app = express()
 
 app.use(bodyParser.urlencoded({
