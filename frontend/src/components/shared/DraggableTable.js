c5aed6974917b2f56e1aa48942a65d3db090a2ed
shkfnly <ashoka.finley@gmail.com>
2018-02-26 17:33:19 -0500

    Done

--- frontend/src/components/shared/DraggableTable.js
+++ frontend/src/components/shared/DraggableTable.js
@@ -1,73 +1,4 @@
 import React, { PureComponent } from 'react'
-// import { Table, Icon } from 'antd'
-// import 'antd/dist/antd.css'
-// import dragula from 'dragula'
-// import 'dragula/dist/dragula.css'
-// import './DraggableTable.css'
-//
-// class DraggableTable extends PureComponent {
-//   constructor(props) {
-//     super(props);
-//     this.state = {
-//       data: props.data,
-//       columns: props.columns
-//     }
-//   }
-//   componentWillReceiveProps(np) {
-//     this.setState({data: np.data})
-//   }
-//   componentDidMount() {
-//     const container = document.querySelector('.ant-table-tbody');
-//     const drake = dragula([container], {
-//       moves: (el, container, handle, sibling) => {
-//         this.start = this.getIndexInParent(el);
-//         return true;
-//       },
-//     });
-//
-//     drake.on('drop', (el, target, source, sibling) => {
-//       this.end = this.getIndexInParent(el);
-//       this.props.handleReorder(this.start, this.end);
-//     });
-//   }
-//   getIndexInParent = el => {
-//     return Array.from(el.parentNode.children).indexOf(el);
-//   };
-//
-//   render() {
-//     console.log(this.state.data)
-//     let columns = this.state.columns.map(column => {
-//       return <th>{column.title}</th>
-//     })
-//     let datas = this.state.data.map(task => {
-//       return (
-//         <tr>
-//           <td>{task.description}</td>
-//           <td>{task.percentage}</td>
-//           <td>{task.ethReward}</td>
-//           <td>{task.deleteTask}</td>
-//         </tr>
-//       )
-//     })
-//     let Table = <table>
-//       <tr>
-//         {columns}
-//       </tr>
-//       {datas}
-//     </table>
-//     return (
-//       <div>
-//         {Table}
-//         {/* <Table
-//           columns={this.state.columns}
-//           pagination={false}
-//           dataSource={this.state.data}
-//         /> */}
-//       </div>
-//     );
-//   }
-// }
-// export default DraggableTable;
 import { Table } from 'antd';
 import { DragDropContext, DragSource, DropTarget } from 'react-dnd';
 import HTML5Backend from 'react-dnd-html5-backend';
@@ -134,6 +65,7 @@ let BodyRow = (props) => {
 
 const rowSource = {
   beginDrag(props) {
+    console.log('beginDrag', props.index)
     return {
       index: props.index,
     };
@@ -182,7 +114,7 @@ class DragSortingTable extends React.Component {
       columns: props.columns
     }
   }
-  
+
   componentWillReceiveProps(np) {
     this.setState({data: np.data})
   }
