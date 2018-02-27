5dcceec3057191860229f5f61cde8960da6cd757
shkfnly <ashoka.finley@gmail.com>
2018-02-27 14:40:30 -0500

    update hashing

--- frontend/src/utilities/hashing.js
+++ frontend/src/utilities/hashing.js
@@ -5,12 +5,9 @@ const hashing = {
     // {t: 'string', v: 'Hello!%'}, {t: 'int8', v:-23}, {t: 'address', v: '0x85F43D8a49eeB85d32Cf465507DD71d507100C1d'}
     let objArray = []
     for (let i = 0; i < types.length; i++) {
-      console.log(bytesarray[i])
-      if (typeof bytesarray[i] !== 'undefined') {
-        types[i] === 'uint'
-          ? objArray.push({t: types[i], v: bytesarray[i].toString()})
-          : objArray.push({t: types[i], v: bytesarray[i]})
-      }
+      types[i] === 'uint'
+        ? objArray.push({t: types[i], v: bytesarray[i].toString()})
+        : objArray.push({t: types[i], v: bytesarray[i]})
     }
 
     let hash = soliditySha3(...objArray)
