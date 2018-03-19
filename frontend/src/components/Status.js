0d3aef34763723cb31d66c22bc275e5272ddb249
Jessica Marshall <jessica.marshall14@gmail.com>
2018-03-19 11:29:14 -0400

    reputation box only appears on status page when user can faucet or register

--- frontend/src/components/Status.js
+++ frontend/src/components/Status.js
@@ -86,21 +86,24 @@ export default ({
               </ButtonGroup>
             </div>
           </div>
-          <div style={{backgroundColor: '#F2A35E', padding: 30}}>
-            <div>
-              <h3>Reputation:</h3>
+          { (openFaucet || notRegistered)
+            ? <div style={{backgroundColor: '#F2A35E', padding: 30}}>
+              <div>
+                <h3>Reputation:</h3>
+              </div>
+              <div style={{marginTop: 20}}>
+                <ButtonGroup>
+                  {notRegistered ? <Button color='success' icon='user-add' onClick={register}>
+                    Register
+                  </Button> : null}
+                  {openFaucet ? <Button icon='download' color='success' onClick={faucet}>
+                    Faucet
+                  </Button> : null}
+                </ButtonGroup>
+              </div>
             </div>
-            <div style={{marginTop: 20}}>
-              <ButtonGroup>
-                {notRegistered ? <Button color='success' icon='user-add' onClick={register}>
-                  Register
-                </Button> : null}
-                {openFaucet ? <Button icon='download' color='success' onClick={faucet}>
-                  Faucet
-                </Button> : null}
-              </ButtonGroup>
-            </div>
-          </div>
+            : null
+          }
         </div>
       </div>
     </div>
