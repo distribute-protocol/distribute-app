60ef9642d23156bcd76cf973b49a0a7d7c91213a
jessicagmarshall <jessica.marshall14@gmail.com>
2018-05-31 14:41:07 -0400

    added all users API endpoint

--- server/routes/user.js
+++ server/routes/user.js
@@ -4,19 +4,6 @@ const assert = require('assert')
 const User = require('../models/user')
 
 module.exports = function (app, url) {
-  app.get('/api/users', (req, res) => {
-    console.log('/api/users')
-    User.findOne({account: req.query.account}).exec((err, userStatus) => {
-      assert.equal(err, null)
-      if (userStatus !== null) {
-        console.log(userStatus)
-        res.send(userStatus)
-      } else {
-        res.send({})
-      }
-    })
-  })
-
   app.post('/api/users', (req, res) => {
     console.log('/api/users', req.query)
     let user = new User({
@@ -24,7 +11,6 @@ module.exports = function (app, url) {
       tokenBalance: 0,
       reputationBalance: 0,
       account: req.query.account,
-      // figure out how to get uPort credentials here (do we even need these rn?)
       credentials: req.body,
       projects: {proposed: [], staked: [], active: [], validating: [], voting: [], complete: [], failed: [], expired: []},
       // figure out how to define objects in an array for the mint events to be of a certain type
@@ -36,5 +22,30 @@ module.exports = function (app, url) {
       console.log('user inserted')
     })
     res.end()
+  })
+
+  app.get('/api/users', (req, res) => {
+    console.log('/api/users')
+    if (req.query.account) {
+      User.findOne({account: req.query.account}).exec((err, userStatus) => {
+        assert.equal(err, null)
+        if (userStatus !== null) {
+          console.log(userStatus)
+          res.send(userStatus)
+        } else {
+          res.send({})
+        }
+      })
+    } else {
+      User.find({}).exec((err, allUsers) => {
+        assert.equal(err, null)
+        if (allUsers !== null) {
+          console.log(allUsers)
+          res.send(allUsers)
+        } else {
+          res.send({})
+        }
+      })
+    }
   })
 }
