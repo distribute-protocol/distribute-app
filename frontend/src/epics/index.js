f04044b173accdd972596d021dae0376790b134b
jessicagmarshall <jessica.marshall14@gmail.com>
2018-06-08 15:22:47 -0400

    updated rxjs to v6 THIS WAS SO TERRIBLE I HATE LIBRARIES

--- frontend/src/epics/index.js
+++ frontend/src/epics/index.js
@@ -4,8 +4,8 @@ import { GET_NETWORK_STATUS, GET_USER_STATUS } from '../constants/getters/Status
 import { networkStatusReceived, userStatusReceived } from '../actions/getters/statusGetterActions'
 
 import { combineEpics } from 'redux-observable'
-import Rx from 'rxjs/Rx'
-const { Observable } = Rx
+import { Observable } from 'rxjs'
+import { map, mergeMap } from 'rxjs/operators'
 
 let getConfig = {
   method: 'GET',
@@ -20,19 +20,19 @@ export async function fetchService (url) {
 }
 
 const getNetworkStatusEpic = action$ =>
-  action$.ofType(GET_NETWORK_STATUS)
+  action$.ofType(GET_NETWORK_STATUS).pipe(
   // pull value from database
-    .mergeMap(action => Observable.from(fetchService(`/api/network`))
-      .map(res => Observable.of(res))
-      .map(result => networkStatusReceived(result))
-    )
+    mergeMap(action => Observable.from(fetchService(`/api/network`))),
+    map(res => Observable.of(res)),
+    map(result => networkStatusReceived(result))
+  )
 
 const getUserStatusEpic = action$ =>
-  action$.ofType(GET_USER_STATUS)
+  action$.ofType(GET_USER_STATUS).pipe(
   // pull value from database
-    .mergeMap((action) => Observable.from(fetchService(`/api/user?account=${action.payload}`))
-      .map(res => Observable.of(res))
-      .map(result => userStatusReceived(result))
-    )
+    mergeMap((action) => Observable.from(fetchService(`/api/user?account=${action.payload}`))),
+    map(res => Observable.of(res)),
+    map(result => userStatusReceived(result))
+  )
 
 export default combineEpics(getNetworkStatusEpic, getUserStatusEpic)
