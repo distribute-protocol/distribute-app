097c8ca9c2b1a02c9d14def235f166ceda23aa72
Ariana Freitag <freitagariana@gmail.com>
2018-06-11 17:43:43 -0400

    made sure right things imported, still having variable issues

--- server/logs/projectRegistry.js
+++ server/logs/projectRegistry.js
@@ -1,9 +1,10 @@
 const Web3 = require('web3')
 const web3 = new Web3(new Web3.providers.HttpProvider('http://localhost:8545'))
-const RR = require('../../frontend/src/abi/ProjectRegistry')
+const PR = require('../../frontend/src/abi/ProjectRegistry')
 
 const Network = require('../models/network')
 const User = require('../models/user')
+const Project = require('../models/project')
 
 module.exports = function () {
   // filter for register events
@@ -14,18 +15,18 @@ module.exports = function () {
     topics: [web3.sha3('LogProjectCreated(address,address,uint256,uint256)')]
   })
 
-  proposeProjectFilter.watch(async (error, result) => {
+  projectCreatedFilter.watch(async (error, result) => {
     if (error) console.error(error)
-    let eventParams = result.data
-    let eventParamArr = eventParams.slice(2).match(/.{1,64}/g)
-    let projectAddress = result.topics[1]
-    let account = eventParamArr[2]
-    let projectCost = parseInt(eventParamArr[3], 16)
-    let proposerStake = parseInt(eventParamArr[4])
-    let sender = eventParamArr[5]
+    var eventParams = result.data
+    var eventParamArr = eventParams.slice(2).match(/.{1,64}/g)
+    var projectAddress = result.topics[1]
+    var account = eventParamArr[2]
+    var projectCost = parseInt(eventParamArr[3])
+    var proposerStake = parseInt(eventParamArr[4])
+    var sender = eventParamArr[5]
   })
 
-  let project = new Project(
+  var project = new Project(
     {
       state: 1,
       weiCost: projectCost,
