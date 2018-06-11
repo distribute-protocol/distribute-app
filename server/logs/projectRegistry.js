e7f8e194ed17d0d74ebafec9f77823800506f5f5
Ariana Freitag <freitagariana@gmail.com>
2018-06-11 17:48:30 -0400

    fixed formatting

--- server/logs/projectRegistry.js
+++ server/logs/projectRegistry.js
@@ -17,25 +17,23 @@ module.exports = function () {
 
   projectCreatedFilter.watch(async (error, result) => {
     if (error) console.error(error)
-    var eventParams = result.data
-    var eventParamArr = eventParams.slice(2).match(/.{1,64}/g)
-    var projectAddress = result.topics[1]
-    var account = eventParamArr[2]
-    var projectCost = parseInt(eventParamArr[3])
-    var proposerStake = parseInt(eventParamArr[4])
-    var sender = eventParamArr[5]
+    let eventParams = result.data
+    let eventParamArr = eventParams.slice(2).match(/.{1,64}/g)
+    let projectAddress = result.topics[1]
+    let account = eventParamArr[2]
+    let projectCost = parseInt(eventParamArr[3])
+    let proposerStake = parseInt(eventParamArr[4])
+    let sender = eventParamArr[5]
   })
 
-  var project = new Project(
-    {
-      state: 1,
-      weiCost: projectCost,
-      proposer: account,
-      address: projectAddress,
-      proposerStake: collateral,
-      proposerType: (proposerType ? 'token' : 'reputation')
-    }
-  )
+  let project = new Project({
+    state: 1,
+    weiCost: projectCost,
+    proposer: account,
+    address: projectAddress,
+    proposerStake: collateral,
+    proposerType: (proposerType ? 'token' : 'reputation')
+  })
   project.save(err => {
     if (err) throw Error
     console.log('project details updated')
