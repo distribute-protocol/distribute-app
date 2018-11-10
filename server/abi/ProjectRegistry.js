cdc395a703718e8e500b6b608f0072b39c43708e
anon <sarah.anne.friend@gmail.com>
2018-11-09 19:56:25 -0500

    Restructure abis and begin env setup

--- server/abi/ProjectRegistry.js
+++ server/abi/ProjectRegistry.js
@@ -1,6 +1,6 @@
 // RINKEBY
 // export const ProjectRegistryAddress = '0xef1ac57f437de18c8e5ec168fbe855fc55e238f1'
-let obj = require('./build/ProjectRegistry.json')
+let obj = require('./build/contracts/ProjectRegistry.json')
 
 const ProjectRegistryAddress = obj.networks['5777'].address
 
