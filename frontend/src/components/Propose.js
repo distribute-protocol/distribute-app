e588597429bdf7d7aeec06fc8c10735f2cc7df32
jessicagmarshall <jessica.marshall14@gmail.com>
2018-11-13 13:47:30 -0500

    reordered initiate proposal categories and added a category subsection

--- frontend/src/components/Propose.js
+++ frontend/src/components/Propose.js
@@ -8,7 +8,8 @@ class ProposeForm extends React.Component {
   constructor () {
     super()
     this.state = {
-      collateralType: ''
+      collateralType: '',
+      category: ''
     }
     this.submitHandler = this.submitHandler.bind(this)
   }
@@ -18,18 +19,37 @@ class ProposeForm extends React.Component {
     this.props.form.resetFields()
   }
 
-  chooseCollateral (type) {
-    this.setState({collateralType: type})
-  }
-
   render () {
-    const menu = (
+    const collateralMenu = (
       <Menu>
         <Menu.Item>
-          <a onClick={() => this.chooseCollateral('Tokens')} target='_blank' rel='noopener noreferrer'>Tokens</a>
+          <a onClick={() => this.setState({collateralType: 'Tokens'})} target='_blank' rel='noopener noreferrer'>Tokens</a>
         </Menu.Item>
         <Menu.Item>
-          <a onClick={() => this.chooseCollateral('Reputation')} target='_blank' rel='noopener noreferrer'>Reputation</a>
+          <a onClick={() => this.setState({collateralType: 'Reputation'})} target='_blank' rel='noopener noreferrer'>Reputation</a>
+        </Menu.Item>
+      </Menu>
+    )
+
+    const categoryMenu = (
+      <Menu>
+        <Menu.Item>
+          <a onClick={() => this.setState({category: 'community mesh networks'})} target='_blank' rel='noopener noreferrer'>community mesh networks</a>
+        </Menu.Item>
+        <Menu.Item>
+          <a onClick={() => this.setState({category: 'urban agriculture'})} target='_blank' rel='noopener noreferrer'>urban agriculture</a>
+        </Menu.Item>
+        <Menu.Item>
+          <a onClick={() => this.setState({category: 'land trusts'})} target='_blank' rel='noopener noreferrer'>land trusts</a>
+        </Menu.Item>
+        <Menu.Item>
+          <a onClick={() => this.setState({category: 'sustainable energy systems'})} target='_blank' rel='noopener noreferrer'>sustainable energy systems</a>
+        </Menu.Item>
+        <Menu.Item>
+          <a onClick={() => this.setState({category: 'open source software'})} target='_blank' rel='noopener noreferrer'>open source software</a>
+        </Menu.Item>
+        <Menu.Item>
+          <a onClick={() => this.setState({category: 'other'})} target='_blank' rel='noopener noreferrer'>other</a>
         </Menu.Item>
       </Menu>
     )
@@ -42,6 +62,28 @@ class ProposeForm extends React.Component {
         <Form layout='horizontal'>
           <div style={{marginTop: 20, marginBottom: 40, backgroundColor: 'white', border: '1px solid #989898', display: 'flex', flexDirection: 'column', justifyContent: 'center', marginLeft: '15%', marginRight: '15%'}}>
             <div style={{display: 'flex', justifyContent: 'space-between', marginTop: 40, backgroundColor: '#FAFBFB', border: '1px solid #989898', marginLeft: '10%', marginRight: '10%'}}>
+              <div style={{marginLeft: 15, marginTop: 20}}>
+                <b><p style={{fontFamily: 'Lato', fontSize: 20}}>Project Name:</p></b>
+                <p style={{fontFamily: 'Lato', fontSize: 14}}>Let's give your project a name<br />so that people can easily find it.</p>
+              </div>
+              <div style={{display: 'flex', justifyContent: 'center', alignItems: 'center', marginRight: 50, paddingLeft: '15%', paddingTop: '5%', paddingBottom: '5%'}}>
+                <FormItem>
+                  {getFieldDecorator('name')(<Input style={{maxWidth: 350, borderRadius: 0, border: '1px solid #989898'}} placeholder='' />)}
+                </FormItem>
+              </div>
+            </div>
+            <div style={{display: 'flex', justifyContent: 'space-between', marginTop: 20, backgroundColor: '#FAFBFB', border: '1px solid #989898', marginLeft: '10%', marginRight: '10%'}}>
+              <div style={{marginLeft: 15, marginTop: 20}}>
+                <b><p style={{fontFamily: 'Lato', fontSize: 20}}>Project Description:</p></b>
+                <p style={{fontFamily: 'Lato', fontSize: 14}}>Tell people what you're trying to<br />build. Focus on what you're<br />making.</p>
+              </div>
+              <div style={{display: 'flex', justifyContent: 'center', alignItems: 'center', marginRight: 50, paddingLeft: '15%', paddingTop: '7%', paddingBottom: '4%'}}>
+                <FormItem>
+                  {getFieldDecorator('summary')(<TextArea style={{maxWidth: 350, borderRadius: 0, border: '1px solid #989898'}} rows={10} type='textarea' />)}
+                </FormItem>
+              </div>
+            </div>
+            <div style={{display: 'flex', justifyContent: 'space-between', marginTop: 20, backgroundColor: '#FAFBFB', border: '1px solid #989898', marginLeft: '10%', marginRight: '10%'}}>
               <div style={{marginLeft: 15, marginTop: 20}}>
                 <b><p style={{fontFamily: 'Lato', fontSize: 20}}>Project Image:</p></b>
                 <p style={{fontFamily: 'Lato', fontSize: 14}}>Adding a picture of where the<br />mesh node should go helps<br />people understand what you<br />want to build.</p>
@@ -62,24 +104,15 @@ class ProposeForm extends React.Component {
             </div>
             <div style={{display: 'flex', justifyContent: 'space-between', marginTop: 20, backgroundColor: '#FAFBFB', border: '1px solid #989898', marginLeft: '10%', marginRight: '10%'}}>
               <div style={{marginLeft: 15, marginTop: 20}}>
-                <b><p style={{fontFamily: 'Lato', fontSize: 20}}>Project Name:</p></b>
-                <p style={{fontFamily: 'Lato', fontSize: 14}}>Let's give your project a name<br />so that people can easily find it.</p>
+                <b><p style={{fontFamily: 'Lato', fontSize: 20}}>Project Category:</p></b>
+                <p style={{fontFamily: 'Lato', fontSize: 14}}>Pick a project category to connect with a specific community.<br />You can always update this later.</p>
               </div>
-              <div style={{display: 'flex', justifyContent: 'center', alignItems: 'center', marginRight: 50, paddingLeft: '15%', paddingTop: '5%', paddingBottom: '5%'}}>
-                <FormItem>
-                  {getFieldDecorator('name')(<Input style={{maxWidth: 350, borderRadius: 0, border: '1px solid #989898'}} placeholder='' />)}
-                </FormItem>
-              </div>
-            </div>
-            <div style={{display: 'flex', justifyContent: 'space-between', marginTop: 20, backgroundColor: '#FAFBFB', border: '1px solid #989898', marginLeft: '10%', marginRight: '10%'}}>
-              <div style={{marginLeft: 15, marginTop: 20}}>
-                <b><p style={{fontFamily: 'Lato', fontSize: 20}}>Project Description:</p></b>
-                <p style={{fontFamily: 'Lato', fontSize: 14}}>Tell people what you're trying to<br />build. Focus on what you're<br />making.</p>
-              </div>
-              <div style={{display: 'flex', justifyContent: 'center', alignItems: 'center', marginRight: 50, paddingLeft: '15%', paddingTop: '7%', paddingBottom: '4%'}}>
-                <FormItem>
-                  {getFieldDecorator('summary')(<TextArea style={{maxWidth: 350, borderRadius: 0, border: '1px solid #989898'}} rows={10} type='textarea' />)}
-                </FormItem>
+              <div style={{display: 'flex', flexDirection: 'column', justifyContent: 'center', marginTop: '5%', marginBottom: '5%', marginRight: '7%'}}>
+                <Dropdown overlay={categoryMenu} style={{justifyContent: 'flex-end'}}>
+                  <Button style={{ maxWidth: 350, border: '1px solid #989898', borderRadius: 0, textAlign: 'left' }}>
+                    <Icon type='down' /> { this.state.category === '' ? null : this.state.category }
+                  </Button>
+                </Dropdown>
               </div>
             </div>
             <div style={{display: 'flex', justifyContent: 'space-between', marginTop: 20, backgroundColor: '#FAFBFB', border: '1px solid #989898', marginLeft: '10%', marginRight: '10%'}}>
@@ -122,13 +155,13 @@ class ProposeForm extends React.Component {
                 <b><p style={{fontFamily: 'Lato', fontSize: 20}}>Collateral Type:</p></b>
                 <p style={{fontFamily: 'Lato', fontSize: 14}}>Would you like to stake tokens<br />or clout to create this<br />proposal?</p>
               </div>
-              <div style={{marginTop: '5%', marginBottom: '5%', marginRight: '7%'}}>
-                <Dropdown overlay={menu}>
+              <div style={{display: 'flex', flexDirection: 'column', justifyContent: 'center', marginTop: '5%', marginBottom: '5%', marginRight: '7%'}}>
+                <Dropdown overlay={collateralMenu} style={{justifyContent: 'flex-end'}}>
                   <Button style={{ maxWidth: 350, border: '1px solid #989898', borderRadius: 0, textAlign: 'left' }}>
                     <Icon type='down' /> { this.state.collateralType === '' ? null : this.state.collateralType }
                   </Button>
                 </Dropdown>
-                { this.state.collateralType === '' ? null : <p style={{paddingLeft: 30, paddingTop: 15}}>You will need to contribute amount {this.state.collateralType.toLowerCase()}.</p> }
+                { this.state.collateralType === '' ? null : <p style={{justifyContent: 'flex-end'}}>You will need to contribute amount {this.state.collateralType.toLowerCase()}.</p> }
               </div>
             </div>
             <div style={{display: 'flex', marginTop: 40, marginBottom: 40, justifyContent: 'center', alignItems: 'center', marginLeft: '15%', marginRight: '15%'}}>
