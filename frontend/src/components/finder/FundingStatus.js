31cc0251f0bf22be2d4873943e6e9a3b64123f19
jessicagmarshall <jessica.marshall14@gmail.com>
2018-11-19 18:59:37 -0500

    oops did math wrong, it is getting late

--- frontend/src/components/finder/FundingStatus.js
+++ frontend/src/components/finder/FundingStatus.js
@@ -15,7 +15,7 @@ export default (props) => {
   return (
     <div style={{height: 240, width: 500, marginBottom: 5, color: 'black'}}>
       <div style={{height: 20, backgroundColor: 'rgba(218, 218, 218, 0.5)'}} />
-      <p style={{color: color, fontSize: 26, fontFamily: 'Avenir Next', marginTop: 5}}>{props.fundingType === 'Token' ? `$${Math.ceil((parseInt(props.project.weiCost) - parseInt(props.project.weiBal)) * (1 / 10 ** 18) * (parseInt(props.usdPerEth.price))).toFixed(2)}` : parseInt(props.project.reputationCost) - parseInt(props.project.reputationBalance)}</p>
+      <p style={{color: color, fontSize: 26, fontFamily: 'Avenir Next', marginTop: 5}}>{props.fundingType === 'Token' ? `$${Math.ceil((parseInt(props.project.weiBal)) * (1 / 10 ** 18) * (parseInt(props.usdPerEth.price))).toFixed(2)}` : parseInt(props.project.reputationBalance)}</p>
       <p style={{fontSize: 20, fontFamily: 'Lato', marginTop: -15}}>staked of {props.fundingType === 'Token' ? `$${Math.ceil((parseInt(props.project.weiCost)) * (1 / 10 ** 18) * (parseInt(props.usdPerEth.price))).toFixed(2)}` : parseInt(props.project.reputationCost) } goal</p>
       <div style={{display: 'flex', justifyContent: 'space-between'}}>
         <Input style={{fontSize: 20, fontFamily: 'Lato', textAlign: 'center', maxWidth: 100, borderRadius: 0, border: '1px solid #989898'}} placeholder={'15,000'} />
