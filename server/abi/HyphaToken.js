f0e67387365062ba6d3cd21a28d45e6e362eb154
shkfnly <ashoka.finley@gmail.com>
2018-12-21 19:09:27 -0200

    Initial logging structure implemented

--- server/abi/HyphaToken.js
+++ server/abi/HyphaToken.js
@@ -1,7 +1,7 @@
 let obj = require('./build/contracts/DistributeToken.json')
-
+//This address lookup is broken...the network id will be different based on where it is being deployed, check this before deployment
 const HyphaTokenAddress = obj.networks['5777'].address
-
+//console.log(Object.keys(obj.networks))
 const HyphaTokenABI = obj.abi
 
 const HyphaTokenBytecode = obj.bytecode
